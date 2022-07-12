0eca53b2cf54fc68105ed1a3b939923eb3c7c5f5
Timothee Odushina <hsp20tim@gmail.com>
2022-07-12 16:14:48 -0600

    added index

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # about-me
+
+## Lab 02
+
+### Author: Timothee Odushina
+
+### Links and Ressources
+
+### Reflections
---
+++ js/app.js
@@ -0,0 +1,45 @@
+'use strict'
+
+//console.log('hvbhbhbhbhb');
+
+let userName = prompt('What is your name ?');
+
+alert(`Welcome to my website ${userName} !! Glad that you are here.`);
+
+let questionOne = prompt('Yes or No...Do you live in the US').toLowerCase();
+
+if(questionOne ==='y' || questionOne === 'yes'){
+alert(`Me too! Nice to meet you ${userName}`)} else if (questionOne === 'n' || questionOne === 'no'){alert(`Feel fee to come visit the US anytime`)} else {alert(`Please answer Yes or No`)};
+
+
+
+
+let questionTwo = prompt('Are you from Seattle').toLowerCase();
+
+if(questionTwo ==='y' || questionTwo === 'yes'){
+alert(`Nice ! I have never been there`)} else if (questionTwo === 'n' || questionTwo === 'no'){alert(`You should go on vacation at Seattle. It is a beautiful city.
+`)} else {alert(`Please answer Yes or No`)};
+
+
+
+let questionThree = prompt('Washington DC or New York City...What is the Capital of United States?').toLowerCase();
+
+if(questionThree ==='Washington DC' || questionThree === 'DC'){
+alert(`You're right. Great JOB !`)} else if (questionThree === 'New York City' || questionThree === 'new york city'){alert(`Wrong Answer! Washington DC is the Capital of the United States
+`)} else {alert(`Please choose between Washington DC or New York City `)};
+
+
+
+
+let questionFour = prompt('Yes or No...Do you have any kids ?').toLowerCase();
+
+if(questionFour ==='y' || questionFour === 'yes'){
+alert(`Congratulations ! I love kids as well`)} else if (questionFour === 'n' || questionFour === 'no'){alert(`Kids can be fun and too much work at the same time`)} else {alert(`Wrong Answer`)};
+
+
+
+
+let questionFive = prompt('Nice talking with you ! Do you wish to view my full page').toLowerCase();
+
+if(questionFive ==='y' || questionFive === 'yes'){
+alert(`Thank you ! Have fun on my page`)} else if (questionFive === 'n' || questionFive === 'no'){alert(`Oh, I do not think you have a choice`)} else {alert(`Wrong Answer ! Enjoy my page`)};
